0ea744259dfd5661c2ce7bbac4d812c9c07adc5d
keerthana021006 <24149024@drngpit.ac.in>
2026-02-04 20:28:53 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 2/4/2026, 8:28:56 PM
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0) return false;
+        int arr[] = {2,3,5};
+        for(int i=0; i<arr.length; i++){
+            while(n%arr[i] == 0){
+                n/=arr[i];
+            }
+        }
+        return n ==1;
+    }
+}
