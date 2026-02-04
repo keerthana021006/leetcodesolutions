c9037365ac512968a1709fb8681716bd7fc783b4
keerthana021006 <24149024@drngpit.ac.in>
2026-02-04 20:27:53 +0530

    Create 2238-ANumberAfterADoubleReversal

---
+++ 2238-ANumberAfterADoubleReversal/2238-ANumberAfterADoubleReversal.java
@@ -0,0 +1,12 @@
+// Last updated: 2/4/2026, 8:27:56 PM
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        if(num == 0)
+            return true;
+        else if(num % 10 == 0)
+            return false;
+        else
+            return true;
+    }
+       
+}
