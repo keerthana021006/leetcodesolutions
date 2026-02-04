48b6b7d733b40e1f552323b82831e8a0eb732888
keerthana021006 <24149024@drngpit.ac.in>
2026-02-04 20:28:23 +0530

    Create 1041-AvailableCapturesForRook

---
+++ 1041-AvailableCapturesForRook/1041-AvailableCapturesForRook.java
@@ -0,0 +1,46 @@
+// Last updated: 2/4/2026, 8:28:25 PM
+class Solution {
+    public int numRookCaptures(char[][] arr) {
+          int strike=0;
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                if(arr[i][j]=='R'){
+                    //move rook to up
+                    for(int k=i-1;k>=0;k--){
+                        if(arr[k][j]=='B') break;
+                        else if(arr[k][j]=='p'){
+                            strike++;
+                            break;
+                        }
+                    }
+                    //move rook to down
+                    for(int k=i+1;k<8;k++){
+                        if(arr[k][j]=='B') break;
+                        else if(arr[k][j]=='p'){
+                            strike++;
+                            break;
+                        }
+                    }
+                    //move rook to left
+                    for(int k=j-1;k>=0;k--){
+                        if(arr[i][k]=='B') break;
+                        else if(arr[i][k]=='p'){
+                            strike++;
+                            break;
+                        }
+                    }
+                    //move rook to right;
+                    for(int k=j+1;k<8;k++){
+                        if(arr[i][k]=='B') break;
+                        else if(arr[i][k]=='p'){
+                            strike++;
+                            break;
+                        }
+                    }
+                    
+                }
+            }
+        } 
+     return strike; 
+    }
+}
